0b6f57041bef3575845d052d0fd583ce78f9246c
furkotikoo <furkanyildiz47@gmail.com>
2024-10-27 21:41:03 +0300

    DisplayError component updated to accept errorMessage

--- src/utils/DisplayError.tsx
+++ src/utils/DisplayError.tsx
@@ -2,27 +2,36 @@ import { ErrorValidation } from "@/Types/ApiResponseType";
 
 // Define the prop types for DisplayError
 interface DisplayErrorProps {
-  errorsValidation: ErrorValidation[] | null;
-  keyProp: string; // Use a different name for the key prop to avoid conflicts with the special `key` in JSX
+  errorsValidation?: ErrorValidation[] | null;
+  keyProp?: string; // Use a different name for the key prop to avoid conflicts with the special `key` in JSX
+  errorMessage?: string | null;
 }
 
 const DisplayError: React.FC<DisplayErrorProps> = ({
   errorsValidation,
   keyProp,
+  errorMessage,
 }) => {
   return (
     <>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       {errorsValidation &&
         errorsValidation.length > 0 &&
         errorsValidation.map((errorObj, index) =>
-          errorObj[keyProp] ? (
+          errorObj[keyProp!] ? (
             <p key={`${keyProp}-error-${index}`} style={{ color: "red" }}>
-              {errorObj[keyProp]}
+              {errorObj[keyProp!]}
             </p>
           ) : null
         )}
     </>
   );
+};
+
+DisplayError.defaultProps = {
+  errorsValidation: [],
+  keyProp: "",
+  errorMessage: "",
 };
 
 export default DisplayError;
