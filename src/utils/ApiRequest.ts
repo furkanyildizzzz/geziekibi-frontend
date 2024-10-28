1358683af86ab8a5ee1ed8ec0cda150c38e74b4b
furkotikoo <furkanyildiz47@gmail.com>
2024-10-28 23:52:44 +0300

    api request commits were deleted

--- src/utils/ApiRequest.ts
+++ src/utils/ApiRequest.ts
@@ -7,38 +7,6 @@ import Cookies from "js-cookie";
 import { isTokenExpired } from "./IsTokenExpired";
 import { useRouter } from "next/router";
 
-// utils/api.ts
-// export async function apiRequest<T>(
-//   url: string,
-//   method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
-//   body?: any
-// ): Promise<ApiResponse<T>> {
-//   try {
-//     const headers: HeadersInit = {
-//       "Content-Type": "application/json",
-//     };
-
-//     const response = await fetch(`http://localhost:4000/v1/${url}`, {
-//       method,
-//       headers,
-//       body: body ? JSON.stringify(body) : undefined,
-//     });
-
-//     const jsonResponse = await response.json();
-//     console.log({ response });
-//     console.log({ jsonResponse });
-//     console.log({ IsErrorResponse: IsErrorResponse(jsonResponse) });
-//     if (IsErrorResponse(jsonResponse)) {
-//       throw jsonResponse;
-//     }
-
-//     return jsonResponse as ApiSuccessResponse<T>;
-//   } catch (error) {
-//     console.error("API Request Failed:", error);
-//     throw error;
-//   }
-// }
-
 const IsErrorResponse = (obj: Response): obj is ApiErrorResponse => {
   console.log({ obj });
   return "errorType" in obj;
