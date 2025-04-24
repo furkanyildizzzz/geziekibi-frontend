adb0e033ea1401db8559c42db3c379032f91de72
Furkan Yıldız <furkanyildiz47@gmail.com>
2025-04-24 13:37:33 +0300

    modal butons responsiveness

--- src/Components/Modal/ModalButtons.tsx
+++ src/Components/Modal/ModalButtons.tsx
@@ -22,51 +22,36 @@ export const ModalButtons: React.FC<ModalButtonsProps> = ({
     if (deleteFunction) deleteFunction();
   };
   return (
-    <Row>
-      <Col>
-        <Row>
-          {deleteFunction && (
-            <Col xs="3">
-              <Button
-                color="danger"
-                onClick={handleDelete}
-                disabled={isLoading}
-              >
-                {" "}
-                {t("Delete")}
-              </Button>
-            </Col>
-          )}
-        </Row>
+    <Row className="mb-3 d-flex justify-content-between">
+      <Col xs="auto">
+        {deleteFunction && (
+          <Button color="danger" onClick={handleDelete} disabled={isLoading}>
+            {t("Delete")}
+          </Button>
+        )}
       </Col>
-      <Col>
-        <Row>
-          <Col xs="6">
-            <Button
-              color="light"
-              onClick={() => {
-                router.back();
-              }}
-              disabled={isLoading}
-            >
-              {" "}
-              {t("Cancel")}
-            </Button>
-          </Col>
-          <Col xs="6">
-            <Button
-              className="ms-2"
-              color="primary"
-              type="submit"
-              disabled={isLoading}
-            >
-              {" "}
-              <div className="d-flex align-items-center gap-sm-2 gap-1">
-                {isLoading ? <LoadingButton /> : t("Submit")}
-              </div>
-            </Button>
-          </Col>
-        </Row>
+
+      <Col xs="auto">
+        <Button
+          color="light"
+          onClick={() => router.back()}
+          disabled={isLoading}
+        >
+          {t("Cancel")}
+        </Button>
+      </Col>
+
+      <Col xs="auto">
+        <Button
+          className="ms-2"
+          color="primary"
+          type="submit"
+          disabled={isLoading}
+        >
+          <div className="d-flex align-items-center gap-sm-2 gap-1">
+            {isLoading ? <LoadingButton /> : t("Submit")}
+          </div>
+        </Button>
       </Col>
     </Row>
   );
